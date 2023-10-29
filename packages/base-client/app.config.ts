832b13f162967199ce01db08e99e7fdcbe8df433
Rihan Arfan <me@file.properties>
2023-10-29 17:36:52 +0000

    feat(base-client): :sparkles: title bar

---
+++ packages/base-client/app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  ui: {
+    primary: "blue",
+    gray: "zinc",
+    input: {
+      color: {
+        white: {
+          outline:
+            "shadow-sm bg-white dark:bg-gray-900 text-gray-900 dark:text-white ring-1 ring-inset ring-gray-300 dark:ring-gray-700",
+        },
+      },
+    },
+  },
+});
---
+++ packages/base-client/components/TitleBar.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { appWindow } from "@tauri-apps/api/window";
+
+defineProps({
+  title: {
+    type: String,
+    default: "Application",
+  },
+});
+</script>
+
+<template>
+  <header class="flex justify-between items-center" data-tauri-drag-region>
+    <div class="flex my-3.5 mx-4 gap-3 items-center">
+      <img src="../public/icon.png" alt="icon" class="h-5 w-5" />
+
+      <h1 class="text-[0.8rem] text-zinc-800 font-medium">
+        {{ title }}
+      </h1>
+    </div>
+
+    <div class="w-1/3"><slot name="center" /></div>
+
+    <div>
+      <UButton
+        icon="i-fluent-subtract-16-regular"
+        size="lg"
+        color="gray"
+        variant="ghost"
+        :ui="{
+          rounded: '',
+          icon: { size: { lg: 'h-4 w-6 my-1.5' } },
+          color: { gray: { ghost: 'hover:bg-[#e9e9e9]' } },
+        }"
+        @click="appWindow.minimize()"
+      />
+
+      <UButton
+        icon="i-fluent-restore-16-regular"
+        size="lg"
+        color="gray"
+        variant="ghost"
+        :ui="{ rounded: '', icon: { size: { lg: 'h-4 w-6 my-1.5' } } }"
+        class="hover:bg-[#e9e9e9]"
+        @click="appWindow.maximize()"
+      />
+
+      <UButton
+        icon="i-fluent-dismiss-24-regular"
+        size="lg"
+        color="gray"
+        variant="ghost"
+        :ui="{
+          rounded: '',
+          icon: { size: { lg: 'h-4 w-6 my-1.5' } },
+          color: { gray: { ghost: 'hover:bg-[#e81123] hover:text-white' } },
+        }"
+        @click="appWindow.close()"
+      />
+    </div>
+  </header>
+</template>
