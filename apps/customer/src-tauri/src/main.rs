109a436ca14fdeb2f6e19313a77fa30d6cc49922
Rihan Arfan <me@file.properties>
2023-12-02 22:41:58 +0000

    feat: :sparkles: windows 11 rounded window + shadow

--- apps/customer/src-tauri/src/main.rs
+++ apps/customer/src-tauri/src/main.rs
@@ -1,8 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+use window_shadows::set_shadow;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        // windows 11 rounded ui
+        // @see https://github.com/tauri-apps/window-shadows/blob/dev/examples/tauri/src-tauri/src/main.rs
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            set_shadow(&window, true).expect("Unsupported platform!");
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
