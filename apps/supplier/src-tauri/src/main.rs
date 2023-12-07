861761ddb9c4e251eb464fd3eb6ae7317b9af09d
Rihan Arfan <me@file.properties>
2023-12-07 03:31:40 +0000

    feat(supplier): :sparkles: tcp connection

--- apps/supplier/src-tauri/src/main.rs
+++ apps/supplier/src-tauri/src/main.rs
@@ -1,9 +1,64 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+    sync::mpsc,
+};
+
 use serde_json::json;
-use tauri::Manager;
+use tauri::{AppHandle, Manager};
 use window_shadows::set_shadow;
+
+use lazy_static::lazy_static;
+use std::sync::{Arc, Mutex};
+
+lazy_static! {
+    static ref STREAM: Arc<Mutex<Option<TcpStream>>> = Arc::new(Mutex::new(None));
+    static ref TX: Mutex<Option<mpsc::Sender<String>>> = Mutex::new(None);
+}
+
+// payload struct for incoming messages from the server
+#[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
+struct Payload {
+    code: String,
+    data: serde_json::Value,
+}
+
+async fn socketHandler(app: AppHandle, rx: mpsc::Receiver<String>) {
+    let mut stream = TcpStream::connect("localhost:5555").unwrap();
+
+    loop {
+        let mut buf = [0; 1024];
+
+        // check if there's a payload in the channel
+        if let Ok(payload) = rx.try_recv() {
+            println!("Payload: {}", payload);
+            // write payload to the stream
+            stream.write_all(payload.as_bytes()).unwrap();
+        }
+
+        // read from the stream
+        let n = stream.read(&mut buf).unwrap();
+
+        if n == 0 {
+            // connection closed, handle this case separately
+            println!("Connection closed");
+            continue;
+        }
+
+        // ignore heartbeat messages
+        if String::from_utf8_lossy(&buf[..n]) == "HEARTBEAT\n" {
+            continue;
+        }
+
+        let msg = String::from_utf8_lossy(&buf[..n]);
+        println!("Message: {}", msg);
+
+        let _ = app.emit_all("server", msg);
+    }
+}
 
 fn main() {
     tauri::Builder::default()
@@ -12,6 +67,13 @@ fn main() {
         .setup(|app| {
             let window = app.get_window("main").unwrap();
             set_shadow(&window, true).expect("Unsupported platform!");
+
+            let (tx, rx) = mpsc::channel();
+            *TX.lock().unwrap() = Some(tx);
+
+            let app_handle = app.handle();
+            tauri::async_runtime::spawn(async move { socketHandler(app_handle, rx).await });
+
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
@@ -37,7 +99,9 @@ fn auth(email: String, password: String) {
         }
     });
 
-    println!("Payload: {}", payload);
+    let tx = TX.lock().unwrap();
+    let tx = tx.as_ref().unwrap();
+    tx.send(payload.to_string()).unwrap();
 }
 
 #[tauri::command]
@@ -49,7 +113,9 @@ fn create_broadcast(message: &str) {
         }
     });
 
-    println!("Payload: {}", payload);
+    let tx = TX.lock().unwrap();
+    let tx = tx.as_ref().unwrap();
+    tx.send(payload.to_string()).unwrap();
 }
 
 #[tauri::command]
@@ -63,7 +129,9 @@ fn create_product(id: String, name: String, icon: String) {
         }
     });
 
-    println!("Payload: {}", payload);
+    let tx = TX.lock().unwrap();
+    let tx = tx.as_ref().unwrap();
+    tx.send(payload.to_string()).unwrap();
 }
 
 #[tauri::command]
@@ -77,7 +145,9 @@ fn edit_product(id: String, name: String, icon: String) {
         }
     });
 
-    println!("Payload: {}", payload);
+    let tx = TX.lock().unwrap();
+    let tx = tx.as_ref().unwrap();
+    tx.send(payload.to_string()).unwrap();
 }
 
 #[tauri::command]
@@ -92,7 +162,9 @@ fn create_variant(product_id: String, size: String, colour: String, quantity: i3
         }
     });
 
-    println!("Payload: {}", payload);
+    let tx = TX.lock().unwrap();
+    let tx = tx.as_ref().unwrap();
+    tx.send(payload.to_string()).unwrap();
 }
 
 #[tauri::command]
@@ -108,7 +180,9 @@ fn edit_variant(product_id: String, id: String, size: String, colour: String, qu
         }
     });
 
-    println!("Payload: {}", payload);
+    let tx = TX.lock().unwrap();
+    let tx = tx.as_ref().unwrap();
+    tx.send(payload.to_string()).unwrap();
 }
 
 #[tauri::command]
@@ -121,5 +195,7 @@ fn send_message(customer_id: String, message: String) {
         }
     });
 
-    println!("Payload: {}", payload);
+    let tx = TX.lock().unwrap();
+    let tx = tx.as_ref().unwrap();
+    tx.send(payload.to_string()).unwrap();
 }
